e4b7d90e1ef2229910c39e798d6aaaad4ac4ff79
Cristian <33324829+CristianJianu@users.noreply.github.com>
2023-03-28 23:48:22 +0300

    final - 2 portocale pe pin1 si pin2, cu difuzor

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showString("start!")
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P1)) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+        music.playTone(247, music.beat(BeatFraction.Half))
+        music.playTone(294, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Half))
+    }
+    if (input.pinIsPressed(TouchPin.P2)) {
+        basic.showLeds(`
+            . . # # #
+            . # # . .
+            # . . # .
+            # . . # .
+            . # # . .
+            `)
+        music.playTone(587, music.beat(BeatFraction.Half))
+        music.playTone(698, music.beat(BeatFraction.Half))
+        music.playTone(880, music.beat(BeatFraction.Half))
+    }
+})
